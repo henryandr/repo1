bc8a28c7f37b66c005ebd89bb464610f98d8053f
Henry Andrade <henry.andrade@upb.edu.co>
2024-07-18 16:36:22 -0500

    Agregué función para imprimir 10 veces hola

--- calculadora.py
+++ calculadora.py
@@ -1,1 +1,8 @@
 print('Hola mundo')
+#Vamos a agregar un for para imprimir los números del 1 al 10
+for i in range(1,11):
+    print(i)    
+
+#escribe una funcion que calcule el area de un cuadrado
+def area_cuadrado(lado):
+    return lado*lado
